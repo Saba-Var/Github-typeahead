3b8843f912b16857ad3d2916e0962648534c8c88
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-02-21 16:30:05 +0400

    create InputField component and move input from Typeahed.js into InputField.js

---
+++ src/Components/InputField/InputField.js
@@ -0,0 +1,27 @@
+import styles from "./InputField.module.css";
+
+const InputField = (props) => {
+  const inputHandler = (e) => {
+    props.setInput(e.target.value);
+  };
+  const usersListVisibility = () => {
+    props.setUsersList(true);
+    props.logoVisibility(true);
+  };
+  const inputLeaveHandler = () => {
+    props.logoVisibility(false);
+  };
+  return (
+    <input
+      type="text"
+      placeholder="username"
+      spellCheck="false"
+      onChange={inputHandler}
+      className={`${props.input !== "" && styles["bottom_corners"]}`}
+      onClick={usersListVisibility}
+      onBlur={inputLeaveHandler}
+    />
+  );
+};
+
+export default InputField;
