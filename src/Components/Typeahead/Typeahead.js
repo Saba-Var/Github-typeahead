1e7357b3e31faf2e2185de31211e20aa966bb1b7
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-02-21 21:57:10 +0400

    add SVG icon on the input field

--- src/Components/Typeahead/Typeahead.js
+++ src/Components/Typeahead/Typeahead.js
@@ -3,7 +3,7 @@ import Users from "../Users/Users";
 import { useState } from "react";
 import Overlay from "../Overlay/Overlay";
 import InputField from "../InputField/InputField";
-
+import closeSVG from "../../assets/close_svg.svg";
 const Typeahead = (props) => {
   const [input, setInput] = useState("");
   const [usersList, setUsersList] = useState(true);
@@ -14,15 +14,25 @@ const Typeahead = (props) => {
     <>
       <Overlay setUsersList={setUsersList} />
       <form onSubmit={submitHandler}>
-        <div className={styles.search}>
-          <InputField
-            logoVisibility={props.setLogoVisibility}
-            setUsersList={setUsersList}
-            setInput={setInput}
-            input={input}
-          />
-          <Users user={input} usersListVisibility={usersList} />
+        <div className={styles.container}>
+          <div className={styles.search}>
+            <InputField
+              logoVisibility={props.setLogoVisibility}
+              setUsersList={setUsersList}
+              setInput={setInput}
+              input={input}
+            />
+            <div className={styles["img__container"]}>
+              <img
+                src={closeSVG}
+                className={styles["close__svg"]}
+                alt="close icon"
+                draggable="false"
+              />
+            </div>
+          </div>
         </div>
+        <Users user={input} usersListVisibility={usersList} />
       </form>
     </>
   );
