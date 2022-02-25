ff902b12824f475bc882b114f3d0afb358ca6f2c
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-02-25 23:17:25 +0400

    change error message, waiting time and favicon

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -3,7 +3,7 @@ const Error = (props) => {
   return (
     <div className={styles["error_message__container"]}>
       <p className={styles["error_message"]}>
-        Exceed 10 requests per minute. Try after a few seconds.
+        Exceed Github API rate limit. Try again in a few seconds.
       </p>
     </div>
   );
