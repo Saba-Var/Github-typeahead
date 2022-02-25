627bf63c849cbdd2896cb8808455d297d5b10a9c
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-02-25 19:54:00 +0400

    test: fetch token from firebase

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -7,13 +7,33 @@ const Users = (props) => {
   const [usersData, setUsersData] = useState([]);
   const [hasError, setError] = useState(false);
   const username = props.user.toLowerCase().trim();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch(
+        "https://github-token-default-rtdb.europe-west1.firebasedatabase.app/token.json"
+      );
+      const data = await response.json();
+      setToken(data.token);
+    }
+    getToken();
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
+      console.log(token);
       if (username !== "") {
         async function fetchGithubAPI() {
           try {
             const response = await fetch(
-              `https://api.github.com/search/users?q=${username}`
+              `https://api.github.com/search/users?q=${username}`,
+              {
+                method: "get",
+                headers: new Headers({
+                  Authorization: `token ${token}`,
+                }),
+              }
             );
             if (response.status === 403) throw new Error("403");
             const data = await response.json();
@@ -40,7 +60,7 @@ const Users = (props) => {
         }
         fetchGithubAPI();
       }
-    }, 1000);
+    }, 1);
     if (username === "") {
       props.setFound(true);
       setUsersData([]);
