004eeed68c47efa09dc7cac29ea3a4b277c68582
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-02-28 13:32:46 +0400

    add Cursor component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Typeahead from "./Components/Typeahead/Typeahead";
 import Card from "./Components/Card/Card";
 import GithubLogo from "./Components/GithubLogo/GithubLogo";
-
+import Cursor from "./Components/Cursor/Cursor";
 function App() {
   const [logoVisibility, setLogoVisibility] = useState(false);
 
@@ -10,6 +10,7 @@ function App() {
     <Card>
       <GithubLogo logoVisibility={logoVisibility} />
       <Typeahead setLogoVisibility={setLogoVisibility} />
+      <Cursor />
     </Card>
   );
 }
