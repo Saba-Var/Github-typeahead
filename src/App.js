28ed92e424e208b2bb9996e8e7ef4aad65d2cdf1
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-02-28 20:03:01 +0400

    cursor changes depend on device

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Cursor from "./Components/Cursor/Cursor";
 
 function App() {
   const [logoVisibility, setLogoVisibility] = useState(false);
+  if (window.innerWidth > 900)
+    document.querySelector("html").style.cursor = "none";
   return (
     <Card>
       <GithubLogo logoVisibility={logoVisibility} />
       <Typeahead setLogoVisibility={setLogoVisibility} />
-      <Cursor />
+      {window.innerWidth > 900 && <Cursor />}
     </Card>
   );
 }
